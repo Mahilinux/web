ae2318f481eb905d0fe854685c5254277a8296dd
Mahilinux <mahi.linux@gmail.com>
2019-06-29 23:33:25 +0530

    git-client file

---
+++ git-client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum install git curl -y >/dev/null 2&>1
+RTCD=`echo $?`
+if [ $RTCD != 0 ]; then
+echo "Git client did not installed"
+fi
+
+# Configure git
+curl -L https://bootstrap.saltstack.com -o install_salt.sh 
+sh /root/install_salt.sh -M
+git clone https://github.com/Mahilinux/srv.git
+mv /root/srv* /srv
