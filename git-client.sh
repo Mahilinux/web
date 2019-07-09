2215f8c28f02014e5442c97b72bc215e0e7ac389
Mahesh <mahi.linux@gmail.com>
2019-07-09 08:24:13 +0530

    Update git-client.sh

--- git-client.sh
+++ git-client.sh
@@ -8,7 +8,7 @@ fi
 # Configure git
 curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh 
 sh /tmp/install_salt.sh -M
-git clone https://github.com/Mahilinux/srv.git /tmp/srv
+git clone https://github.com/mahi-linux/srv.git /tmp/srv
 mv /tmp/srv/* /srv
 rm -rf /tmp/srv
 rm -rf /tmp/install_salt.sh
